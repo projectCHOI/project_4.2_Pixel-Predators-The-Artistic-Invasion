145107b73ec71fd7f31f6c83fad715a670d69ce7
yoonsuk <yoonsuk.choi93@gmail.com>
2024-05-13 23:38:12 +0900

    .

--- 1_The Artistic Invasion.py
+++ 1_The Artistic Invasion.py
@@ -1,13 +1,24 @@
-# pip install pygame 인스톨!
 import pygame
+
 pygame.init()
-win = pygame.display.set_mode((500, 500))
-pygame.display.set_caption("Dodge a Red Box")
+
+# 윈도우 설정
+win = pygame.display.set_mode((1000, 500))
+pygame.display.set_caption("The Artistic Invasion")
+
+# 배경 이미지 불러오기
+background = pygame.image.load(r"C:\Users\HOME\Desktop\새싹_교육\GitHub_CHOI\project_4.2_Pixel Predators-The Artistic Invasion\project4.2_world\WorldGrassland-J.jpg")
 
 run = True
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    # 배경 이미지를 화면에 그리기
+    win.blit(background, (0, 0))
+
+    # 화면 업데이트
+    pygame.display.update()
 
 pygame.quit()
