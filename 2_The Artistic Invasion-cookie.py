a4f181a3f4352c0d47a3aa91e12d72fbd475d3ae
yoonsuk <yoonsuk.choi93@gmail.com>
2024-05-26 23:27:56 +0900

    초기

---
+++ 2_The Artistic Invasion-cookie.py
@@ -0,0 +1,12 @@
+import pygame
+pygame.init()
+win = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("Dodge a Red Box")
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
