a755a7a0b94a8fe4789febdfd562db78d0a196a1
YoonSukChoi <yoonsuk.choi93@gmail.com>
2024-10-13 00:30:42 +0900

    direction

--- M_title_stage_images/main.py
+++ M_title_stage_images/main.py
@@ -619,7 +619,7 @@ while run:
             length = math.hypot(direction[0], direction[1])
             if length == 0:
                 continue
-            direction = (direction[0] / length * attack_speed, direction[1] / length * attack_speed)
+            direction = (direction[0] / length * attack_speed * delta_time, direction[1] / length * attack_speed * delta_time)
             new_end = (start[0] + direction[0], start[1] + direction[1])
             if 0 <= new_end[0] <= win_width and 0 <= new_end[1] <= win_height:
                 new_attacks.append((new_end, (new_end[0] + direction[0], new_end[1] + direction[1]), thickness))
