0d68d7ba9a351f37b76fe930723fba94489ed1f1
YoonSukChoi <yoonsuk.choi93@gmail.com>
2025-05-08 01:21:42 +0900

    enemy_behaviors

--- M_title_stage_images/enemy_behaviors/bomb.py
+++ M_title_stage_images/enemy_behaviors/bomb.py
@@ -1,1 +1,10 @@
 # bomb
+def generate(level, win_width, win_height):
+    """
+    내용
+    """
+    enemies = []
+    """
+    내용
+    """
+    return enemies
--- M_title_stage_images/enemy_behaviors/move_and_disappear.py
+++ M_title_stage_images/enemy_behaviors/move_and_disappear.py
@@ -1,1 +1,10 @@
 # move_and_disappear
+def generate(level, win_width, win_height):
+    """
+    내용
+    """
+    enemies = []
+    """
+    내용
+    """
+    return enemies
