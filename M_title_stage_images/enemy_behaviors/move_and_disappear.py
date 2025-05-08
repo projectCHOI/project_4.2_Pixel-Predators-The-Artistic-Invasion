4575db443ba0b604bab382b25815d88457a1a067
YoonSukChoi <yoonsuk.choi93@gmail.com>
2025-05-09 01:03:40 +0900

    mob_enemy_Relentless

--- M_title_stage_images/enemy_behaviors/move_and_disappear.py
+++ M_title_stage_images/enemy_behaviors/move_and_disappear.py
@@ -17,28 +17,37 @@ def load_image(*path_parts, size=None):
     return img
 
 SIZE = 40
-
-enemy_image_up = load_image("enemies", "mob_enemy_Relentless Charger_1.png", size=(SIZE, SIZE))
-enemy_image_down = load_image("enemies", "mob_enemy_Relentless Charger_2.png", size=(SIZE, SIZE))
+enemy_image_up    = load_image("enemies", "mob_enemy_Relentless Charger_1.png", size=(SIZE, SIZE))
+enemy_image_down  = load_image("enemies", "mob_enemy_Relentless Charger_2.png", size=(SIZE, SIZE))
+enemy_image_left  = load_image("enemies", "mob_enemy_Relentless Charger_3.png", size=(SIZE, SIZE))
+enemy_image_right = load_image("enemies", "mob_enemy_Relentless Charger_4.png", size=(SIZE, SIZE))
 
 def generate(level, win_width, win_height):
 
     enemies = []
 
+    # 스테이지별 속도 및 개수 설정
     speed = random.randint(10, 12)
     num_enemies = random.randint(3, 8)
 
-    directions = [(0, 1), (0, -1)]
+    # 사방향 이동 지원
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
     for _ in range(num_enemies):
         direction = random.choice(directions)
-        # 위에서 아래로 이동
-        if direction == (0, 1):
+        # 방향에 따라 시작 위치와 이미지 선택
+        if direction == (0, 1):  # 위에서 아래
             pos = [random.randint(0, win_width - SIZE), 0]
             image = enemy_image_up
-        else:
-            # 아래에서 위로 이동
+        elif direction == (0, -1):  # 아래에서 위
             pos = [random.randint(0, win_width - SIZE), win_height - SIZE]
             image = enemy_image_down
+        elif direction == (1, 0):  # 왼쪽에서 오른쪽
+            pos = [0, random.randint(0, win_height - SIZE)]
+            image = enemy_image_right
+        else:  # (-1, 0) 오른쪽에서 왼쪽
+            pos = [win_width - SIZE, random.randint(0, win_height - SIZE)]
+            image = enemy_image_left
 
         enemies.append([
             pos,              # [x, y]
