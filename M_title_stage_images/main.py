40da7cf1f1786f2e8fab1d6841b7060c53f9e56f
YoonSukChoi <yoonsuk.choi93@gmail.com>
2025-12-25 00:39:23 +0900

    hp

--- M_title_stage_images/main.py
+++ M_title_stage_images/main.py
@@ -91,9 +91,8 @@ collision_images = {
 
 # Health 설정
 health_image = load_image("player", "mob_Life.png", size=image_size)
-#HP 임시수정
-max_health = 10
-current_health = 10
+max_health = 3
+current_health = 5
 
 # 스피드 아이템 설정
 speed_item_image = load_image("items", "mob_item_Slowly_2.PNG", size=image_size)
