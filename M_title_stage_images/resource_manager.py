39a6e3a84bf5651f2981f5edd2fede698743ead0
YoonSukChoi <yoonsuk.choi93@gmail.com>
2026-03-04 00:27:03 +0900

    pygame

--- M_title_stage_images/resource_manager.py
+++ M_title_stage_images/resource_manager.py
@@ -1,1 +1,17 @@
 # 이미지, 사운드, 폰트 로딩 전용
+import pygame
+import os
+
+class ResourceManager:
+    def __init__(self):
+        self.base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        self.image_dir = os.path.join(self.base_dir, "M_title_stage_images", "assets", "images")
+        self.sound_dir = os.path.join(self.base_dir, "M_title_stage_images", "assets", "sounds")
+        self.font_dir = os.path.join(self.base_dir, "M_title_stage_images", "assets", "fonts")
+
+    def load_image(self, *path_parts, size=None):
+        path = os.path.join(self.image_dir, *path_parts)
+        image = pygame.image.load(path).convert_alpha()
+        if size:
+            image = pygame.transform.scale(image, size)
+        return image
