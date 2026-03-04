2f8a807e2e2e6bd77a01cfd35ee9a6cb4262e580
YoonSukChoi <yoonsuk.choi93@gmail.com>
2026-03-05 00:10:53 +0900

    Player

--- M_title_stage_images/entities/player.py
+++ M_title_stage_images/entities/player.py
@@ -18,3 +18,40 @@ class Player(pygame.sprite.Sprite):
             2: self.res.load_image("player", "mob_death_2.png", size=self.size),
             1: self.res.load_image("player", "mob_death_3.png", size=self.size)
         }
+
+        self.image = self.img_right
+        self.rect = self.image.get_rect(center=(WIN_WIDTH // 2, WIN_HEIGHT // 2))
+        
+        # 상태 변수
+        self.pos = list(self.rect.center)
+        self.speed = 10
+        self.health = PLAYER_START_HEALTH
+        self.max_health = PLAYER_MAX_HEALTH
+        
+        # 무적 및 효과 관련
+        self.invincible = False
+        self.invincible_start_time = 0
+        self.invincible_duration = INVINCIBLE_DURATION
+        self.current_collision_img = None
+        self.collision_effect_start_time = 0
+        self.collision_effect_duration = 0
+
+    def handle_input(self, input_reversed=False):
+        keys = pygame.key.get_pressed()
+        move_mult = -1 if input_reversed else 1
+        dx, dy = 0, 0
+
+        if keys[pygame.K_a]: dx -= self.speed * move_mult
+        if keys[pygame.K_d]: dx += self.speed * move_mult
+        if keys[pygame.K_w]: dy -= self.speed * move_mult
+        if keys[pygame.K_s]: dy += self.speed * move_mult
+
+        # 위치 업데이트 및 화면 밖 탈출 방지
+        self.pos[0] = max(0, min(self.pos[0] + dx, WIN_WIDTH - self.size[0]))
+        self.pos[1] = max(0, min(self.pos[1] + dy, WIN_HEIGHT - self.size[1]))
+        self.rect.topleft = self.pos
+
+        # 이미지 방향 전환
+        if dx < 0: self.image = self.img_left
+        elif dx > 0: self.image = self.img_right
+
