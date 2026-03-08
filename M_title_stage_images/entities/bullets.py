87321387124a0cee00dde56fb5734fc5bada5ee4
YoonSukChoi <yoonsuk.choi93@gmail.com>
2026-03-09 00:45:57 +0900

    Bullet

--- M_title_stage_images/entities/bullets.py
+++ M_title_stage_images/entities/bullets.py
@@ -1,3 +1,16 @@
 # 공격 및 에너지볼 클래스
 import pygame
 from M_title_stage_images.config import *
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, pos, color, speed=15):
+        super().__init__()
+        self.image = pygame.Surface((10, 10))
+        self.image.fill(color)
+        self.rect = self.image.get_rect(center=pos)
+        self.speed = speed
+
+    def update(self):
+        self.rect.y -= self.speed
+        if self.rect.bottom < 0:
+            self.kill()
