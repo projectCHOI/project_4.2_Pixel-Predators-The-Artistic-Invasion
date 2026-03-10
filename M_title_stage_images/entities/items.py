b0cbb07d5dd1f021b2ba920d26330a67c746bfaf
YoonSukChoi <yoonsuk.choi93@gmail.com>
2026-03-11 00:11:12 +0900

    Item

--- M_title_stage_images/entities/items.py
+++ M_title_stage_images/entities/items.py
@@ -2,3 +2,9 @@
 import pygame
 import random
 from M_title_stage_images.config import *
+
+class Item(pygame.sprite.Sprite):
+    def __init__(self, pos, item_type, res_manager):
+        super().__init__()
+        self.res = res_manager
+        self.type = item_type
