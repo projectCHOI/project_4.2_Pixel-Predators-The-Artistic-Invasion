47b6d0e215c6953fa4d5f443cc0d0308fe7f2b60
YoonSukChoi <yoonsuk.choi93@gmail.com>
2026-03-12 00:53:20 +0900

    Item

--- M_title_stage_images/entities/items.py
+++ M_title_stage_images/entities/items.py
@@ -8,3 +8,13 @@ class Item(pygame.sprite.Sprite):
         super().__init__()
         self.res = res_manager
         self.type = item_type
+
+        if self.type == 'heal':
+            self.image = self.res.load_image("items", "heal_item.png", size=(30, 30))
+        elif self.type == 'power':
+            self.image = self.res.load_image("items", "power_item.png", size=(30, 30))
+        elif self.type == 'speed':
+            self.image = self.res.load_image("items", "speed_item.png", size=(30, 30))
+            
+        self.rect = self.image.get_rect(center=pos)
+        self.speed = 3
