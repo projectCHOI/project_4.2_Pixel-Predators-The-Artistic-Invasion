57782da55f0176f79f326821427d6d22fb41852b
최윤석 <144885127+projectCHOI@users.noreply.github.com>
2026-03-14 21:17:42 +0900

    game_manager

--- M_title_stage_images/game_manager.py
+++ M_title_stage_images/game_manager.py
@@ -1,3 +1,14 @@
 # 스테이지 관리, 게임 상태(Over/Victory) 관리
 import pygame
 from M_title_stage_images.config import *
+
+class GameManager:
+    def __init__(self, res_manager):
+        self.res = res_manager
+        self.level = 1
+        self.max_level = 9
+        
+        # 게임 상태 관리
+        self.game_active = False
+        self.game_over = False
+        self.game_over_reason = None # "victory", "game_over", "time_over"
