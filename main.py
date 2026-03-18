a73bfedc602a122b542832254f0c2a9f6f4f8728
YoonSukChoi <yoonsuk.choi93@gmail.com>
2026-03-19 00:49:59 +0900

    pygame

--- main.py
+++ main.py
@@ -4,9 +4,21 @@ import math
 import os
 import sys
 
-# 1. 분리된 모듈들 임포트
+# 모듈 임포트
 from M_title_stage_images.config import *
 from M_title_stage_images.resource_manager import ResourceManager
 from M_title_stage_images.game_manager import GameManager
 from M_title_stage_images.entities.player import Player
 from M_title_stage_images.assets.sounds.bgm_controller import BGMController
+
+# 초기화
+pygame.init()
+win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+pygame.display.set_caption("The Artistic Invasion")
+clock = pygame.time.Clock()
+
+# 매니저 및 리소스 로더 초기화
+res = ResourceManager()
+manager = GameManager(res)
+player = Player(res)
+bgm = BGMController()
