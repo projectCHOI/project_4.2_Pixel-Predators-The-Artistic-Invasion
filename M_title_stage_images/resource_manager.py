fbc01058d23d9c26cccae3c0f9e962b1d394a86d
YoonSukChoi <yoonsuk.choi93@gmail.com>
2026-04-22 00:17:15 +0900

    pygame

--- M_title_stage_images/resource_manager.py
+++ M_title_stage_images/resource_manager.py
@@ -13,3 +13,31 @@ WIN_WIDTH = 800
 WIN_HEIGHT = 600
 FPS = 60
 WHITE = (255, 255, 255)
+
+def main():
+    # 2. 초기화
+    pygame.init()
+    pygame.mixer.init()
+    
+    win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    pygame.display.set_caption("The Artistic Invasion - Resource Ready")
+    clock = pygame.time.Clock()
+
+    res = ResourceManager()
+
+    # 메인 루프
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        win.fill(WHITE)
+
+        pygame.display.update()
+        clock.tick(FPS)
+
+    pygame.quit()
+    sys.exit()
+
+if __name__ == "__main__":
+    main()
