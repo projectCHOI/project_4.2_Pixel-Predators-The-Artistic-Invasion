bcc2f3aaddeebdb1e3f73f96ed7283215046bd46
YoonSukChoi <yoonsuk.choi93@gmail.com>
2026-04-24 01:12:29 +0900

    main

--- main.py
+++ main.py
@@ -14,3 +14,43 @@ WIN_WIDTH = 1280
 WIN_HEIGHT = 720
 FPS = 60
 WHITE = (255, 255, 255)
+
+def main():
+    pygame.init()
+    pygame.mixer.init()
+    
+    win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    pygame.display.set_caption("The Artistic Invasion")
+    clock = pygame.time.Clock()
+
+
+    res = ResourceManager()
+
+    game_state = 0 
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    game_state = 1
+
+        if game_state == 0:
+            win.blit(title_image, (0, 0))
+        else:
+            if stage_background_images:
+                win.blit(stage_background_images[0], (0, 0))
+            else:
+                win.fill(WHITE)
+
+        pygame.display.update()
+        clock.tick(FPS)
+
+    pygame.quit()
+    sys.exit()
+
+if __name__ == "__main__":
+    main()
