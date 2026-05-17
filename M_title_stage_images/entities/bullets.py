253e7abd0089bed862540ecdcc1e736ad7257949
YoonSukChoi <yoonsuk.choi93@gmail.com>
2026-05-17 21:07:17 +0900

    bullet

--- M_title_stage_images/entities/bullets.py
+++ M_title_stage_images/entities/bullets.py
@@ -4,43 +4,16 @@ import math
 from M_title_stage_images.config import *
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, pos, color, target_pos): # target_pos 추가
+    def __init__(self, pos, color, target_pos):
         super().__init__()
-        self.image = pygame.Surface((10, 10), pygame.SRCALPHA)
-        pygame.draw.circle(self.image, color, (5, 5), 5)
+        # 반지름 5인 빨간색/지정색 작은 원 생성
+        self.radius = 5
+        self.image = pygame.Surface((self.radius * 2, self.radius * 2), pygame.SRCALPHA)
+        pygame.draw.circle(self.image, color, (self.radius, self.radius), self.radius)
         self.rect = self.image.get_rect(center=pos)
         
         self.speed = 15
-        # 방향 계산 로직
-        mx, my = target_pos
-        px, py = pos
-        angle = math.atan2(my - py, mx - px) # 각도 계산
         
-        self.vx = math.cos(angle) * self.speed
-        self.vy = math.sin(angle) * self.speed
-        
-        self.x = float(self.rect.x)
-        self.y = float(self.rect.y)
-
-    def update(self):
-        self.x += self.vx
-        self.y += self.vy
-        self.rect.x = int(self.x)
-        self.rect.y = int(self.y)
-
-        if not (0 <= self.rect.x <= WIN_WIDTH and 0 <= self.rect.y <= WIN_HEIGHT):
-            self.kill()
-class EnergyBall(pygame.sprite.Sprite):
-    def __init__(self, pos, res_manager, target_pos): # target_pos 추가
-        super().__init__()
-        self.res = res_manager
-        # 에너지볼 이미지 로드 (경로 확인 필수)
-        self.image = self.res.load_image("player", "mob_me_ball.png", size=(30, 30))
-        self.rect = self.image.get_rect(center=pos)
-        
-        self.speed = 8
-        
-        # 방향 계산
         mx, my = target_pos
         px, py = pos
         angle = math.atan2(my - py, mx - px)
@@ -57,5 +30,6 @@ class EnergyBall(pygame.sprite.Sprite):
         self.rect.x = int(self.x)
         self.rect.y = int(self.y)
 
-        if not (0 <= self.rect.x <= WIN_WIDTH and 0 <= self.rect.y <= WIN_HEIGHT):
-            self.kill()        
+        if (self.rect.bottom < 0 or self.rect.top > WIN_HEIGHT or 
+            self.rect.right < 0 or self.rect.left > WIN_WIDTH):
+            self.kill()
